3f4623d9d973141b048de8e6c12245c5ed8d81bd
Tanjil Pranto <53004677+tanjil072@users.noreply.github.com>
2020-08-02 15:16:35 +0600

    Google Maps Added

--- Screen/Components/TabScreens/HomeScreen.js
+++ Screen/Components/TabScreens/HomeScreen.js
@@ -1,15 +1,52 @@
-
-import React from 'react';
-
-//Import all required component
-import { View, Text } from 'react-native';
-
-const HomeScreen = () => {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', marginTop: 100 }}>
-      <Text style={{ fontSize: 23, marginTop: 10 }}>Home Screen</Text>
-      
-    </View>
-  );
-};
-export default HomeScreen;
+import React, { Component } from 'react';  
+import { StyleSheet, View } from 'react-native';  
+import MapView from 'react-native-maps';  
+import { Marker } from 'react-native-maps';  
+  
+export default class App extends Component {  
+  render() {  
+    return (  
+      <View style={styles.MainContainer}>  
+  
+        <MapView  
+          style={styles.mapStyle}  
+          showsUserLocation={false}  
+          zoomEnabled={true}  
+          zoomControlEnabled={true}  
+          initialRegion={{  
+            latitude: 23.6850,   
+            longitude: 90.3563,  
+            latitudeDelta: .010,  
+            longitudeDelta: 1.0,  
+          }}>  
+  
+          <Marker  
+            coordinate={{ latitude: 23.8103, longitude: 90.4125 }}  
+            title={"Dhaka"}  
+            description={"The capital of Bangladesh"}  
+          />  
+        </MapView>  
+          
+      </View>  
+    );  
+  }  
+}  
+  
+const styles = StyleSheet.create({  
+  MainContainer: {  
+    position: 'absolute',  
+    top: 0,  
+    left: 0,  
+    right: 0,  
+    bottom: 0,  
+    alignItems: 'center',  
+    justifyContent: 'flex-end',  
+  },  
+  mapStyle: {  
+    position: 'absolute',  
+    top: 0,  
+    left: 0,  
+    right: 0,  
+    bottom: 0,  
+  },  
+});  
