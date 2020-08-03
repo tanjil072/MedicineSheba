1a03cff9e895501e181292901e8af5f0f9286c6b
Tanjil Pranto <53004677+tanjil072@users.noreply.github.com>
2020-08-03 22:10:36 +0600

    update profile
    dynamically add/edit text

--- Screen/Components/TabScreens/ProfileScreen.js
+++ Screen/Components/TabScreens/ProfileScreen.js
@@ -12,124 +12,141 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import Dialog from "react-native-dialog";
 
 
-var edittext;
+const text = ""; //Dialogue Title setter
+
 
 export default class ProfileScreen extends Component {
 
   state = {
-    dialogVisible: false
+    dialogVisible: false,
+    Email:"tanjilh84@gmail.com",
+    Phone:"+88017********",
+    Password:"********"
   };
 
-  signout =() =>{
+  signout = () => {
     this.props.navigation.navigate('LoginScreen');
   }
-  showDialog = () => {
+  showDialog = (mail) => {
+    this.text = mail;
     this.setState({ dialogVisible: true });
+
   };
 
   handleCancel = () => {
     this.setState({ dialogVisible: false });
   };
 
-  handleDelete = () => {
-    // The user has pressed the "Delete" button, so here you can do your own logic.
-    // ...Your logic
+  handleSubmit = () => {
+    //alert(this.Text2)
     this.setState({ dialogVisible: false });
   };
 
-
-  render(){
-
-  return (
-
-
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <View style={styles.headerContent}>
-          <Image style={styles.avatar}
-            source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar7.png' }} />
-
-          <Text style={styles.name}>Tanjil Pranto </Text>
-          <Text style={styles.userInfo}>tanjilh84@gmail.com </Text>
-        </View>
-      </View>
-
-      <View style={styles.body}>
-
-        <View style={styles.detailBox}>
-          <View style={styles.box1}>
-            <Text style={{ fontSize: 20, color: 'white' }}>Username</Text>
-            <Text style={{ fontSize: 20, color: '#BEBEBE' }}>Tanjil</Text>
-          </View>
-          
-        </View>
+  handleText = (textinp,ref) => {
+    if(ref=='Email'){
+      this.setState({ Email: textinp });
+    }
+    if(ref=='Phone'){
+      this.setState({ Phone: textinp });
+    }
+    if(ref=='Password'){
+      this.setState({ Password: textinp });
+    }
+    
+  };
 
 
+  render() {
+
+    return (
 
 
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <View style={styles.headerContent}>
+            <Image style={styles.avatar}
+              source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar7.png' }} />
 
-        <View style={styles.detailBox}>
-          <View style={styles.box1}>
-            <Text style={{ fontSize: 20, color: 'white' }}>Email</Text>
-            <Text style={{ fontSize: 20, color: '#BEBEBE' }}>Tanjilh84@gmail.com</Text>
-          </View>
-          <View style={styles.box2}>
-            <Icon name="create" size={25} style={{ marginTop: 25 }} onPress={this.showDialog} />
+            <Text style={styles.name}>Tanjil Pranto </Text>
+            <Text style={styles.userInfo}>{this.state.Email}</Text>
           </View>
         </View>
 
-        <View style={styles.detailBox}>
-          <View style={styles.box1}>
-            <Text style={{ fontSize: 20, color: 'white' }}>Phone</Text>
-            <Text style={{ fontSize: 20, color: '#BEBEBE' }}>+88017********</Text>
-          </View>
-          <View style={styles.box2}>
-            <Icon name="create" size={25} style={{ marginTop: 25 }} onPress={this.showDialog}  />
-          </View>
-        </View>
+        <View style={styles.body}>
 
-        <View style={styles.detailBox}>
-          <View style={styles.box1}>
-            <Text style={{ fontSize: 20, color: 'white' }}>Password</Text>
-            <Text style={{ fontSize: 20, color: '#BEBEBE' }}>********</Text>
-          </View>
-          <View style={styles.box2}>
-            <Icon name="create" size={25} style={{ marginTop: 25 }} onPress={this.showDialog}  />
-          </View>
-        </View>
+          <View style={styles.detailBox}>
+            <View style={styles.box1}>
+              <Text style={{ fontSize: 20, color: 'white' }}>Username</Text>
+              <Text style={{ fontSize: 20, color: '#BEBEBE' }}>Tanjil</Text>
+            </View>
 
-        <View style={styles.item}>
+          </View>
 
-          <TouchableOpacity
-            style={styles.buttonStyle}
-            activeOpacity={0.5}
-            onPress={this.signout}
+
+
+
+
+          <View style={styles.detailBox}>
+            <View style={styles.box1}>
+              <Text style={{ fontSize: 20, color: 'white' }}>Email</Text>
+              <Text style={{ fontSize: 20, color: '#BEBEBE' }}>{this.state.Email}</Text>
+            </View>
+            <View style={styles.box2}>
+              <Icon name="create" size={25} style={{ marginTop: 5 }} onPress={() => { this.showDialog('Email') }} />
+            </View>
+          </View>
+
+          <View style={styles.detailBox}>
+            <View style={styles.box1}>
+              <Text style={{ fontSize: 20, color: 'white' }}>Phone</Text>
+              <Text style={{ fontSize: 20, color: '#BEBEBE' }}>{this.state.Phone}</Text>
+            </View>
+            <View style={styles.box2}>
+              <Icon name="create" size={25} style={{ marginTop: 5 }} onPress={() => { this.showDialog('Phone') }} />
+            </View>
+          </View>
+
+          <View style={styles.detailBox}>
+            <View style={styles.box1}>
+              <Text style={{ fontSize: 20, color: 'white' }}>Password</Text>
+              <Text style={{ fontSize: 20, color: '#BEBEBE' }}>{this.state.Password}</Text>
+            </View>
+            <View style={styles.box2}>
+              <Icon name="create" size={25} style={{ marginTop: 5 }} onPress={() => { this.showDialog('Password') }} />
+            </View>
+          </View>
+
+          <View style={styles.item}>
+
+            <TouchableOpacity
+              style={styles.buttonStyle}
+              activeOpacity={0.5}
+              onPress={this.signout}
             >
 
-            <Text style={styles.buttonTextStyle}>Sign Out</Text>
-          </TouchableOpacity>
+              <Text style={styles.buttonTextStyle}>Sign Out</Text>
+            </TouchableOpacity>
 
+
+          </View>
+
+          <View>
+
+            <Dialog.Container visible={this.state.dialogVisible}>
+              <Dialog.Title>Edit {this.text}</Dialog.Title>
+
+              <Dialog.Input style={{ borderWidth: 1, borderRadius: 15, paddingLeft: 10 }} placeholder={this.text}  onChangeText={(textinp) => this.handleText(textinp,this.text)} >
+
+              </Dialog.Input>
+              <Dialog.Button label="Cancel" onPress={this.handleCancel} />
+              <Dialog.Button label="Submit" onPress={this.handleSubmit} />
+
+            </Dialog.Container>
+          </View>
 
         </View>
-
-        <View>
-        {/* <TouchableOpacity onPress={this.showDialog}>
-          <Text>Show Dialog</Text>
-        </TouchableOpacity> */}
-        <Dialog.Container visible={this.state.dialogVisible}>
-          <Dialog.Title>Edit Email</Dialog.Title>
-          
-          <Dialog.Input style={{borderWidth:1,borderRadius:15,paddingLeft:10}} placeholder="Email">
-            
-          </Dialog.Input>
-          <Dialog.Button label="Cancel" onPress={this.handleCancel} />
-          <Dialog.Button label="Submit" onPress={this.handleDelete} />
-        </Dialog.Container>
       </View>
-
-      </View>
-    </View>
-  );
+    );
   }
 
 }
