31f9ce47e89d1fe5670b22ed2d5ad617a086dab8
Tanjil Pranto <53004677+tanjil072@users.noreply.github.com>
2020-08-06 01:44:34 +0600

    flat list added with search

--- Screen/Components/BuyerScreens/NotificationScreen.js
+++ Screen/Components/BuyerScreens/NotificationScreen.js
@@ -1,24 +1,130 @@
-import * as React from 'react';
-import { View } from 'react-native';
-import { RadioButton, Text } from 'react-native-paper';
+//This is an example code to Add Search Bar Filter on Listview//
+import React, { Component } from 'react';
+//import react in our code.
 
-const MyComponent = () => {
-  const [value, setValue] = React.useState('first');
+import {
+  Text,
+  StyleSheet,
+  View,
+  FlatList,
+  TextInput,
+  ActivityIndicator,
+  Alert,
+} from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+//import all the components we are going to use.
 
-  return (
-    <RadioButton.Group onValueChange={value => setValue(value)} value={value} >
-      <View style={{ flex: 1, flexDirection: 'row',alignItems:"center",justifyContent:"center" }}>
-        <View>
-          <Text>First</Text>
-          <RadioButton value="first" />
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    //setting default state
+    this.state = { isLoading: true, text: '' };
+    this.arrayholder = [];
+  }
+
+  componentDidMount() {
+    return fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState(
+          {
+            isLoading: false,
+            dataSource: responseJson
+          },
+          function() {
+            this.arrayholder = responseJson;
+          }
+        );
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+  SearchFilterFunction(text) {
+    //passing the inserted text in textinput
+    const newData = this.arrayholder.filter(function(item) {
+      //applying filter for the inserted text in search bar
+      const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase();
+      const textData = text.toUpperCase();
+      return itemData.indexOf(textData) > -1;
+    });
+    this.setState({
+      //setting the filtered newData on datasource
+      //After setting the data it will automatically re-render the view
+      dataSource: newData,
+      text: text,
+    });
+  }
+  ListViewItemSeparator = () => {
+    //Item sparator view
+    return (
+      <View
+        style={{
+          height: 0.3,
+          width: '90%',
+          backgroundColor: '#080808',
+        }}
+      />
+    );
+  };
+  a(id){
+    alert(id)
+  }
+  render() {
+    if (this.state.isLoading) {
+      //Loading View while data is loading
+      return (
+        <View style={{ flex: 1, paddingTop: 20 }}>
+          <ActivityIndicator />
         </View>
-        <View>
-          <Text>Second</Text>
-          <RadioButton value="second" />
-        </View>
+      );
+    }
+    return (
+      //ListView to show with textinput used as search bar
+      <View style={styles.viewStyle}>
+        <TextInput
+          style={styles.textInputStyle}
+          onChangeText={text => this.SearchFilterFunction(text)}
+          value={this.state.text}
+          underlineColorAndroid="transparent"
+          placeholder="Search Here"
+        />
+        <FlatList
+          data={this.state.dataSource}
+          ItemSeparatorComponent={this.ListViewItemSeparator}
+          renderItem={({ item }) => (
+            <View>
+              <TouchableOpacity onPress={() => alert("clicked")}>
+              <Text>{item.id} </Text>
+            <Text onPress={() => alert(item.id)} style={styles.textStyle}>{item.title} </Text>
+            </TouchableOpacity>
+            </View>
+            
+            
+          )}
+          enableEmptySections={true}
+          style={{ marginTop: 10 }}
+          keyExtractor={(item, index) => index.toString()}
+        />
       </View>
-    </RadioButton.Group>
-  );
-};
-
-export default MyComponent;
+    );
+  }
+}
+const styles = StyleSheet.create({
+  viewStyle: {
+    justifyContent: 'center',
+    flex: 1,
+    marginTop: 40,
+    padding: 16,
+  },
+  textStyle: {
+    padding: 10,
+  },
+  textInputStyle: {
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 10,
+    borderColor: '#009688',
+    backgroundColor: '#FFFFFF',
+  },
+});
