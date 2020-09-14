dd24e9a62b38fca993aad918f846f9bb51385af9
Tanjil Pranto <53004677+tanjil072@users.noreply.github.com>
2020-09-14 20:33:15 +0600

    update

---
+++ Screen/Components/BuyerScreens/test2.js
@@ -0,0 +1,178 @@
+import React from 'react';
+import { StyleSheet, Button, Text, View, TouchableOpacity, ScrollView, Image, ActivityIndicator, TextInput, Alert } from 'react-native';
+//import { MaterialIcons, AntDesign, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export default class Cart extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectAll: false,
+			cartItemsIsLoading: false,
+			cartItems: [
+				/* Sample data from walmart */
+				{ itemId: "501436323", name: "Power Wheels Dune Racer Extreme", thumbnailImage: "https://i5.walmartimages.com/asr/a3922e8e-2128-4603-ba8c-b58d1333253b_1.44d66337098c1db8fed9abe2ff4b57ce.jpeg?odnHeight=100&odnWidth=100&odnBg=FFFFFF", color: "Red", qty: 1, salePrice: "105", checked: 1 },
+				{ itemId: "35031861", name: "Better Homes & Gardens Leighton Twin Over Twin Wood Bunk Bed, Multiple Finishes", thumbnailImage: "https://i5.walmartimages.com/asr/4aedb609-4b61-4593-ad8a-cdc8c88696b1_1.3f505ce3d55db4745cf4c51d559994dc.jpeg?odnHeight=100&odnWidth=100&odnBg=FFFFFF", qty: 1, color: "Green", salePrice: "199", checked: 0 },
+				{ itemId: "801099131", name: "LEGO Star Wars 2019 Advent Calendar 75245 Holiday Building Kit", thumbnailImage: "https://i5.walmartimages.com/asr/9a8ea1ab-311d-455c-bda8-ce15692a8185_3.208d48e0260f80891d32b351cb116a4b.jpeg?odnHeight=100&odnWidth=100&odnBg=FFFFFF", qty: 1, color: "Blue", salePrice: "27.99", checked: 0 },
+				{ itemId: "42608079", name: "Little Tikes Cape Cottage Playhouse, Tan", thumbnailImage: "https://i5.walmartimages.com/asr/2654cd64-1471-44af-8b0c-1debaf598cb3_1.c30c481d1ac8fdd6aa041c0690d7214c.jpeg?odnHeight=100&odnWidth=100&odnBg=FFFFFF", color: "Purple", qty: 1, salePrice: "129.99", checked: 0 },
+				{ itemId: "247714372", name: "HP 14\" Laptop, Intel Core i3-1005G1, 4GB SDRAM, 128GB SSD, Pale Gold, 14-DQ1038wm", thumbnailImage: "https://i5.walmartimages.com/asr/b442f6e7-c5e1-4387-9cd9-9205811d4980_1.82b94d1c11dd12a6697bc517219f331e.jpeg?odnHeight=100&odnWidth=100&odnBg=FFFFFF", qty: 1, color: "Black", salePrice: "269", checked: 0 }
+			]
+		}
+	}
+
+	selectHandler = (index, value) => {
+		const newItems = [...this.state.cartItems]; // clone the array 
+		newItems[index]['checked'] = value == 1 ? 0 : 1; // set the new value 
+		
+		this.setState({ cartItems: newItems }); // set new state
+	}
+
+	selectHandlerAll = (value) => {
+		const newItems = [...this.state.cartItems]; // clone the array 
+		newItems.map((item, index) => {
+			newItems[index]['checked'] = value == true ? 0 : 1; // set the new value 
+		});
+		this.setState({ cartItems: newItems, selectAll: (value == true ? false : true) }); // set new state
+	}
+
+	deleteHandler = (index) => {
+		Alert.alert(
+			'Are you sure you want to delete this item from your cart?',
+			'',
+			[
+				{ text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+				{
+					text: 'Delete', onPress: () => {
+						let updatedCart = this.state.cartItems; /* Clone it first */
+						updatedCart.splice(index, 1); /* Remove item from the cloned cart state */
+						this.setState(updatedCart); /* Update the state */
+					}
+				},
+			],
+			{ cancelable: false }
+		);
+	}
+
+	quantityHandler = (action, index) => {
+		const newItems = [...this.state.cartItems]; // clone the array 
+
+		let currentQty = newItems[index]['qty'];
+
+		if (action == 'more') {
+			newItems[index]['qty'] = currentQty + 1;
+		} else if (action == 'less') {
+			newItems[index]['qty'] = currentQty > 1 ? currentQty - 1 : 1;
+		}
+
+		this.setState({ cartItems: newItems }); // set new state
+	}
+
+	subtotalPrice = () => {
+		const { cartItems } = this.state;
+		if (cartItems) {
+			return cartItems.reduce((sum, item) => sum + (item.checked == 1 ? item.qty * item.salePrice : 0), 0);
+		}
+		return 0;
+	}
+
+	itemname=()=>{
+		
+		alert(this.subtotalPrice().toFixed(2));
+	}
+
+
+
+	render() {
+		const styles = StyleSheet.create({
+			centerElement: { justifyContent: 'center', alignItems: 'center' },
+		});
+
+		const { cartItems, cartItemsIsLoading, selectAll } = this.state;
+
+
+		return (
+			<View style={{ flex: 1, backgroundColor: '#f6f6f6' }}>
+				<View style={{ flexDirection: 'row', backgroundColor: '#fff', marginBottom: 10 }}>
+					<View style={[styles.centerElement, { width: 50, height: 50 }]}>
+						<Icon style={[{ color: "black" }]} size={25} name={'cart'} />
+					</View>
+					<View style={[styles.centerElement, { height: 50 }]}>
+						<Text style={{ fontSize: 18, color: '#000' }}>Cart</Text>
+					</View>
+				</View>
+
+				{cartItemsIsLoading ? (
+					<View style={[styles.centerElement, { height: 300 }]}>
+						<ActivityIndicator size="large" color="#ef5739" />
+					</View>
+				) : (
+
+
+						<ScrollView>
+							{cartItems && cartItems.map((item, i) => (
+								<View key={i} style={{ flexDirection: 'row', backgroundColor: '#fff', marginBottom: 2, height: 120 }}>
+									<View style={[styles.centerElement, { width: 60 }]}>
+										<TouchableOpacity style={[styles.centerElement, { width: 32, height: 32 }]} onPress={() => this.selectHandler(i, item.checked)}>
+											<Icon name={item.checked == 1 ? "ios-checkmark-circle" : "ios-checkmark-circle-outline"} size={25} color={item.checked == 1 ? "#0faf9a" : "#aaaaaa"} />
+										</TouchableOpacity>
+									</View>
+									<View style={{ flexDirection: 'row', flexGrow: 1, flexShrink: 1, alignSelf: 'center' }}>
+										<TouchableOpacity onPress={() => {/*this.props.navigation.navigate('ProductDetails', {productDetails: item})*/ }} style={{ paddingRight: 10 }}>
+											<Image source={{ uri: item.thumbnailImage }} style={[styles.centerElement, { height: 60, width: 60, backgroundColor: '#eeeeee' }]} />
+										</TouchableOpacity>
+										<View style={{ flexGrow: 1, flexShrink: 1, alignSelf: 'center' }}>
+											<Text numberOfLines={1} style={{ fontSize: 15 }}>{item.name}</Text>
+											<Text numberOfLines={1} style={{ color: '#8f8f8f' }}>{item.color ? 'Variation: ' + item.color : ''}</Text>
+											<Text numberOfLines={1} style={{ color: '#333333', marginBottom: 10 }}>${item.qty * item.salePrice}</Text>
+											<View style={{ flexDirection: 'row' }}>
+												<TouchableOpacity onPress={() => this.quantityHandler('less', i)} style={{ borderWidth: 1, borderColor: '#cccccc' }}>
+													<Icon name="remove" size={22} color="#cccccc" />
+												</TouchableOpacity>
+												<Text style={{ borderTopWidth: 1, borderBottomWidth: 1, borderColor: '#cccccc', paddingHorizontal: 7, paddingTop: 3, color: '#bbbbbb', fontSize: 13 }}>{item.qty}</Text>
+												<TouchableOpacity onPress={() => this.quantityHandler('more', i)} style={{ borderWidth: 1, borderColor: '#cccccc' }}>
+													<Icon name="add" size={22} color="#cccccc" />
+												</TouchableOpacity>
+											</View>
+										</View>
+
+									</View>
+									<View style={[styles.centerElement, { width: 60 }]}>
+										<TouchableOpacity style={[styles.centerElement, { width: 32, height: 32 }]} onPress={() => this.deleteHandler(i)}>
+											<Icon name="md-trash" size={25} color="#ee4d2d" />
+										</TouchableOpacity>
+									</View>
+								</View>
+							))}
+						</ScrollView>
+					)}
+
+				<View style={{flexDirection: 'row'}}>
+					<View style={[styles.centerElement, { width: 60 }]}>
+						<TouchableOpacity style={[styles.centerElement, { width: 32, height: 32 }]} onPress={() => this.selectHandlerAll(selectAll)}>
+							<Icon style={{ marginTop: 20 }} name={selectAll == true ? "ios-checkmark-circle" : "ios-checkmark-circle-outline"} size={25} color={selectAll == true ? "#0faf9a" : "#aaaaaa"} />
+						</TouchableOpacity>
+					</View>
+
+					<View style={{ flexDirection: 'row', flexGrow: 1, flexShrink: 1, justifyContent: 'space-between', alignItems: 'center' }}>
+						<Text style={{ paddingTop: 20 }}>Select All</Text>
+						<View style={{ flexDirection: 'row', paddingRight: 20, marginTop: -20, alignItems: 'center' }}>
+							<Text style={{ color: '#8f8f8f' }}>SubTotal: </Text>
+							<Text>${this.subtotalPrice().toFixed(2)}</Text>
+						</View>
+					</View>
+				</View>
+
+				<View style={{ flexDirection: 'row', justifyContent: 'flex-end', height: 32, paddingRight: 20, alignItems: 'center' }}>
+					<TouchableOpacity   style={[styles.centerElement, { backgroundColor: '#0faf9a', width: 100, height: 25, borderRadius: 5 }]} onPress={() => this.itemname()}>
+						<Text style={{ color: '#ffffff' }}>Procced</Text>
+					</TouchableOpacity>
+				</View>
+
+			</View>
+
+
+
+		);
+	}
+}
